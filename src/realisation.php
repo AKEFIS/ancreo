1fa212e74034442db2c2372e249f4d6974521b3f
Bonhomme Elouan <bonhommeelouan34@gmail.com>
2025-12-08 11:31:16 +0100

    Fix: Modification de la structure du projet / modification des include de la navbar header et footer / modification des liens pour redirection

--- src/realisation.php
+++ src/realisation.php
@@ -1,4 +1,7 @@
-<?php include('components/header.php') ?>
+<?php include 'components/header.php'; ?>
+<title>Réalisation</title>
+    <script src="js/modal.js" defer></script>
+<?php include 'components/navbar.php'; ?>
   
   <main class="p-4">
 
@@ -98,29 +101,4 @@
       <p class="modal-text text16 text-black leading-relaxed mb-8"></p>
     </div>
   </div>
-
-  <footer class="p-8 bg-black">
-        <div class="flex flex-col gap-5 text20 mb-8 sm:flex-row sm:justify-between sm:gap-0">
-            <ul>
-              <li>Ancreo</li>
-              <li>44 Boulevard Ablert Einstein</li>
-              <li>17000 La Rochelle - France</li>
-              <li>Tél : 06 50 02 17 35</li>
-              <li><a href="mailto:ancreo17@gmail.com">Email : ancreo17@gmail.com</a></li>
-            </ul>
-            <ul>
-              <li><a href="index.html">Accueil</a></li>
-              <li><a href="realisation.html">Réalisation</a></li>
-              <li><a href="services.html">Services</a></li>
-              <li><a href="contact.html">Contact</a></li>
-            </ul>
-            <iframe class="sm:w-[33%]" 
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2764.116873402291!2d-1.1593534242005934!3d46.148418787527866!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x480153b92a04c73b%3A0xdb42e82ff25e37ce!2s44%20Av.%20Albert%20Einstein%2C%2017000%20La%20Rochelle!5e0!3m2!1sfr!2sfr!4v1763989853042!5m2!1sfr!2sfr"
-                width="100%" height="100%" style="border:0;">
-            </iframe>
-        </div>
-        <p class="text16 text-center">Copyright Ancreo © 2025 All rights reserved</p>
-  </footer>
-
-</body>
-</html>
+  <?php include('components/footer.php') ?>
